c69fa80256b33060c3cbd7a58b0d28ad768132dc
Mateusz Burak <mateuszburakemail@gmail.com>
2024-01-30 17:49:17 +0100

    added a dark theme option

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Witam serdecznie");
+let buttonTheme = document.querySelector(".js-buttonTheme");
+let themeName = document.querySelector(".js-themeName");
+let body = document.querySelector(".body");
+let navigation__button = document.querySelector(".navigation__button");
+const section = document.querySelectorAll(".section");
+
+buttonTheme.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    navigation__button.classList.toggle("darkButton");
+    section.forEach((item) =>
+        item.classList.toggle("darkSection"));
+themeName.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
+}
+)
