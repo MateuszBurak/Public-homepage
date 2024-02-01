5cada548b3861cf4e65a3eaa627db050e3b4ade4
Mateusz Burak <mateuszburakemail@gmail.com>
2024-02-01 11:02:27 +0100

    optimized the code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 {
-    const buttonTheme = document.querySelector(".js-buttonTheme");
-    const themeName = document.querySelector(".js-themeName");
-    const body = document.querySelector(".body");
-    const section = document.querySelectorAll(".section");
-
     const toggleDarkTheme = () => {
+
+        const body = document.querySelector(".body");
+
         body.classList.toggle("dark");
+
+        const section = document.querySelectorAll(".section");
+
         section.forEach((item) =>
             item.classList.toggle("darkSection"));
+
+        const themeName = document.querySelector(".js-themeName");
+
         themeName.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
     }
+    const init = () => {
+        const buttonTheme = document.querySelector(".js-buttonTheme");
 
-    buttonTheme.addEventListener("click", toggleDarkTheme)
+        buttonTheme.addEventListener("click", toggleDarkTheme)
+    }
+    init();
 }
