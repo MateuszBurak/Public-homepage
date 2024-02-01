5cf0a6887aa08df72952f29ac8a7e22aad23598a
Mateusz Burak <mateuszburakemail@gmail.com>
2024-02-01 09:57:30 +0100

    changed the code to get rid of global variables, used const instead of let and added a function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-let buttonTheme = document.querySelector(".js-buttonTheme");
-let themeName = document.querySelector(".js-themeName");
-let body = document.querySelector(".body");
-let navigation__button = document.querySelector(".navigation__button");
-const section = document.querySelectorAll(".section");
+{
+    const buttonTheme = document.querySelector(".js-buttonTheme");
+    const themeName = document.querySelector(".js-themeName");
+    const body = document.querySelector(".body");
+    const section = document.querySelectorAll(".section");
 
-buttonTheme.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    section.forEach((item) =>
-        item.classList.toggle("darkSection"));
-themeName.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
+    const toggleDarkTheme = () => {
+        body.classList.toggle("dark");
+        section.forEach((item) =>
+            item.classList.toggle("darkSection"));
+        themeName.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
+    }
+
+    buttonTheme.addEventListener("click", toggleDarkTheme)
 }
-)
